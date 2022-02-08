bfb0caaa4607bbf8e75d7e333511d030f715ac36
hamso <hamsonassor14@gmail.com>
2022-02-08 17:31:28 +0300

    property module start

--- apps/properties/apps.py
+++ apps/properties/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'properties'
+    name = 'apps.properties'
